171750952c6926a50eaf146fea3a7589fc0cefd0
AutomationBlog <sanwin24@gmail.com>
2024-02-25 13:46:35 +0530

    Day 6 task updated

--- Day_6_Task/array_functions.js
+++ Day_6_Task/array_functions.js
@@ -7,55 +7,69 @@ xhr.send();
 xhr.onload = function () {
   countryResponse = JSON.parse(xhr.responseText);
   //console.log(countryResponse);
-  asiaCountries(countryResponse);
+  /* asiaCountries(countryResponse);
   populationCountries(countryResponse);
   countryDetails(countryResponse);
-  totalPopulation(countryResponse);
+  totalPopulation(countryResponse); */
   usDollarCounries(countryResponse);
 };
 
 function asiaCountries(country) {
-  console.log("********** Get all the countries from Asia continent /region using Filter function **********");
-  const asiaCountries = country.filter(country => country.region === 'Asia');
-  asiaCountries.forEach(country => {
+  console.log(
+    "********** Get all the countries from Asia continent /region using Filter function **********"
+  );
+  const asiaCountries = country.filter((country) => country.region === "Asia");
+  asiaCountries.forEach((country) => {
     console.log(country.name.common);
   });
   //console.log("********** *********** **********");
 }
 
 function populationCountries(country) {
-  console.log("********** Get all the countries with a population of less than 2 lakhs using Filter function **********");
-  const popCountries = country.filter(country => country.population > 200000 );
-  popCountries.forEach(country => {
+  console.log(
+    "********** Get all the countries with a population of less than 2 lakhs using Filter function **********"
+  );
+  const popCountries = country.filter((country) => country.population > 200000);
+  popCountries.forEach((country) => {
     console.log(country.name.common);
   });
   //console.log("********** *********** **********");
 }
 function countryDetails(country) {
-  console.log("********** Print the following details name, capital, flag, using forEach function **********");
-  country.forEach(country => {
-    console.log(`============ \n Name: ${country.name.common} \n Capital: ${country.capital} \n Flag: ${country.flag} \n ============`);
+  console.log(
+    "********** Print the following details name, capital, flag, using forEach function **********"
+  );
+  country.forEach((country) => {
+    console.log(
+      `============ \n Name: ${country.name.common} \n Capital: ${country.capital} \n Flag: ${country.flag} \n ============`
+    );
   });
   //console.log("********** *********** **********");
 }
 function totalPopulation(country) {
-  console.log("********** Print the total population of countries using reduce function **********");
+  console.log(
+    "********** Print the total population of countries using reduce function **********"
+  );
   const totalPopulation = country.reduce((value, country) => {
-      return value + country.population;
+    return value + country.population;
   }, 0);
   console.log("Total Population ::: " + totalPopulation);
   //console.log("********** *********** **********");
 }
 
 function usDollarCounries(country) {
-  console.log("********** Print the country that uses US dollars as currency. **********");
-  const usDollarCountries = country.filter(country => {
-    const currencies = Object.values(country.currencies || {});
-    return currencies.includes('USD');
+  console.log(
+    "********** Print the country that uses US dollars as currency. **********"
+  );
+  const usDollarCountries = country.filter(
+    (country) => country.currencies && country.currencies.USD
+  );
+  const usdCountryNames = usDollarCountries.map(
+    (country) => country.name.common
+  );
+  //console.log(...usdCountryNames.join("\n"));
+  usdCountryNames.forEach((country) => {
+    console.log(country);
   });
-  console.log(usDollarCountries.length);
-  /*usDollarCountries.forEach(country => {
-      console.log(country.name.common);
-  });*/
-  //console.log("********** *********** **********");
+  console.log("********** *********** **********");
 }
